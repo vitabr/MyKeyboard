af80a19915135070fcf0d0d4b314a6d4bb887180
Vitaliy A <vitabr@gmail.com>
2016-06-05 23:30:26 +0300

    clean up the code

--- app/src/main/java/com/vito/mykeyboard/services/VitoKeyboard.java
+++ app/src/main/java/com/vito/mykeyboard/services/VitoKeyboard.java
@@ -16,52 +16,23 @@
 
 package com.vito.mykeyboard.services;
 
-import android.app.Dialog;
 import android.inputmethodservice.InputMethodService;
 import android.inputmethodservice.Keyboard;
 import android.inputmethodservice.KeyboardView;
-import android.os.IBinder;
-import android.text.InputType;
-import android.text.method.MetaKeyKeyListener;
-import android.util.Log;
-import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
 import android.view.View;
-import android.view.Window;
 import android.view.inputmethod.CompletionInfo;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.ExtractedTextRequest;
 import android.view.inputmethod.InputConnection;
-import android.view.inputmethod.InputMethodManager;
 import android.view.inputmethod.InputMethodSubtype;
 
 import com.vito.mykeyboard.R;
 import com.vito.mykeyboard.ui.views.VitoKeyboardView;
 import com.vito.mykeyboard.uttil.Parser;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-/**
- * Example of writing an input method for a soft keyboard.  This code is
- * focused on simplicity over completeness, so it should in no way be considered
- * to be a complete soft keyboard implementation.  Its purpose is to provide
- * a basic example for how you would get started writing an input method, to
- * be fleshed out as appropriate.
- */
 public class VitoKeyboard extends InputMethodService
         implements KeyboardView.OnKeyboardActionListener {
-    
-    /**
-     * This boolean indicates the optional example code for performing
-     * processing of hard keys in addition to regular text generation
-     * from on-screen interaction.  It would be used for input methods that
-     * perform language translations (such as converting text entered on 
-     * a QWERTY keyboard to Chinese), but may not be used for input methods
-     * that are primarily intended to be used for on-screen text entry.
-     */
-    static final boolean PROCESS_HARD_KEYS = true;
 
     private VitoKeyboardView mInputView;
     private Keyboard mQwertyKeyboard;
@@ -74,7 +45,7 @@ public class VitoKeyboard extends InputMethodService
     @Override public void onCreate() {
         super.onCreate();
     }
-    
+
     /**
      * This is the point where you can do all of your UI initialization.  It
      * is called after creation and any configuration change.
@@ -82,7 +53,7 @@ public class VitoKeyboard extends InputMethodService
     @Override public void onInitializeInterface() {
         mQwertyKeyboard = new Keyboard(this, R.xml.qwerty);
     }
-    
+
     /**
      * Called by the framework when your view for creating input needs to
      * be generated.  This will be called the first time your input method
@@ -122,13 +93,13 @@ public class VitoKeyboard extends InputMethodService
     @Override public void onFinishInput() {
         super.onFinishInput();
         setCandidatesViewShown(false);
-        
+
         mCurKeyboard = mQwertyKeyboard;
         if (mInputView != null) {
             mInputView.closing();
         }
     }
-    
+
     @Override public void onStartInputView(EditorInfo attribute, boolean restarting) {
         super.onStartInputView(attribute, restarting);
         mInputView.setKeyboard(mCurKeyboard);
@@ -143,8 +114,8 @@ public class VitoKeyboard extends InputMethodService
      * Deal with the editor reporting movement of its cursor.
      */
     @Override public void onUpdateSelection(int oldSelStart, int oldSelEnd,
-            int newSelStart, int newSelEnd,
-            int candidatesStart, int candidatesEnd) {
+                                            int newSelStart, int newSelEnd,
+                                            int candidatesStart, int candidatesEnd) {
         super.onUpdateSelection(oldSelStart, oldSelEnd, newSelStart, newSelEnd,
                 candidatesStart, candidatesEnd);
     }
@@ -157,7 +128,7 @@ public class VitoKeyboard extends InputMethodService
      */
     @Override public void onDisplayCompletions(CompletionInfo[] completions) {
     }
-    
+
     /**
      * Use this to monitor key events being delivered to the application.
      * We get first crack at them, and can either resume them or let them
@@ -165,83 +136,19 @@ public class VitoKeyboard extends InputMethodService
      */
     @Override public boolean onKeyDown(int keyCode, KeyEvent event) {
         switch (keyCode) {
-            case KeyEvent.KEYCODE_BACK:
-                // The InputMethodService already takes care of the back
-                // key for us, to dismiss the input method if it is shown.
-                // However, our keyboard could be showing a pop-up window
-                // that back should dismiss, so we first allow it to do that.
-                if (event.getRepeatCount() == 0 && mInputView != null) {
-                    if (mInputView.handleBack()) {
-                        return true;
-                    }
-                }
-                break;
-                
             case KeyEvent.KEYCODE_DEL:
-                    onKey(Keyboard.KEYCODE_DELETE, null);
-                    return true;
-                
+                onKey(Keyboard.KEYCODE_DELETE, null);
+                return true;
+
             case KeyEvent.KEYCODE_ENTER:
-                    onKey(Keyboard.KEYCODE_DONE, null);
-                    return true;
-                
-            default:
-                // For all other keys, if we want to do transformations on
-                // text being entered with a hard keyboard, we need to process
-                // it and do the appropriate action.
-                if (PROCESS_HARD_KEYS) {
-                    if (keyCode == KeyEvent.KEYCODE_SPACE
-                            && (event.getMetaState()&KeyEvent.META_ALT_ON) != 0) {
-                        // A silly example: in our input method, Alt+Space
-                        // is a shortcut for 'android' in lower case.
-                        InputConnection ic = getCurrentInputConnection();
-                        if (ic != null) {
-                            // First, tell the editor that it is no longer in the
-                            // shift state, since we are consuming this.
-                            ic.clearMetaKeyStates(KeyEvent.META_ALT_ON);
-                            keyDownUp(KeyEvent.KEYCODE_A);
-                            keyDownUp(KeyEvent.KEYCODE_N);
-                            keyDownUp(KeyEvent.KEYCODE_D);
-                            keyDownUp(KeyEvent.KEYCODE_R);
-                            keyDownUp(KeyEvent.KEYCODE_O);
-                            keyDownUp(KeyEvent.KEYCODE_I);
-                            keyDownUp(KeyEvent.KEYCODE_D);
-                            // And we consume this event.
-                            return true;
-                        }
-                    }
-                }
+                onKey(Keyboard.KEYCODE_DONE, null);
+                return true;
         }
-        
+
         return super.onKeyDown(keyCode, event);
     }
 
-    /**
-     * Helper to update the shift state of our keyboard based on the initial
-     * editor state.
-     */
-    private void updateShiftKeyState(EditorInfo attr) {
-        if (attr != null 
-                && mInputView != null && mQwertyKeyboard == mInputView.getKeyboard()) {
-            int caps = 0;
-            EditorInfo ei = getCurrentInputEditorInfo();
-            if (ei != null && ei.inputType != InputType.TYPE_NULL) {
-                caps = getCurrentInputConnection().getCursorCapsMode(attr.inputType);
-            }
-        }
-    }
-    
-    /**
-     * Helper to determine if a given character code is alphabetic.
-     */
-    private boolean isAlphabet(int code) {
-        if (Character.isLetter(code)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-    
+
     /**
      * Helper to send a key down / key up pair to the current editor.
      */
@@ -251,24 +158,7 @@ public class VitoKeyboard extends InputMethodService
         getCurrentInputConnection().sendKeyEvent(
                 new KeyEvent(KeyEvent.ACTION_UP, keyEventCode));
     }
-    
-    /**
-     * Helper to send a character to the editor as raw key events.
-     */
-    private void sendKey(int keyCode) {
-        switch (keyCode) {
-            case '\n':
-                keyDownUp(KeyEvent.KEYCODE_ENTER);
-                break;
-            default:
-                if (keyCode >= '0' && keyCode <= '9') {
-                    keyDownUp(keyCode - '0' + KeyEvent.KEYCODE_0);
-                } else {
-                    getCurrentInputConnection().commitText(String.valueOf((char) keyCode), 1);
-                }
-                break;
-        }
-    }
+
 
     public void onKey(int primaryCode, int[] keyCodes) {
         if (primaryCode == Keyboard.KEYCODE_DONE) {
@@ -300,16 +190,15 @@ public class VitoKeyboard extends InputMethodService
         ic.beginBatchEdit();
         ic.commitText(text, 0);
         ic.endBatchEdit();
-        updateShiftKeyState(getCurrentInputEditorInfo());
     }
 
     private void handleCharacter(int primaryCode, int[] keyCodes) {
 
-            getCurrentInputConnection().commitText(
-                    String.valueOf((char) primaryCode), 1);
+        getCurrentInputConnection().commitText(
+                String.valueOf((char) primaryCode), 1);
 
     }
-    
+
     public void swipeLeft() {
 
     }
@@ -325,10 +214,10 @@ public class VitoKeyboard extends InputMethodService
 
     public void swipeUp() {
     }
-    
+
     public void onPress(int primaryCode) {
     }
-    
+
     public void onRelease(int primaryCode) {
     }
 }
